9206399544cc3fb58e953f87baba67eec6bfee62
Jeremiah Leary <Jeremiah Leary>
2016-12-29 23:31:51 -0600

    Added components to prototype generation.

--- examples/prototype/prot_gen.py
+++ examples/prototype/prot_gen.py
@@ -25,11 +25,20 @@ def main():
     oInterfaceA = xray.interface('xray-interface_a.adoc', 'xray_system')
     oInterfaceB = xray.interface('xray-interface_b.adoc', 'xray_system')
 
+    # Create some components
+    oComponentA = xray.component('xray-component_a.adoc', 'xray_system')
+    oComponentB = xray.component('xray-component_b.adoc', 'xray_system')
+
+    
+
     oDocument.add_object(oInterfaceA)
     oDocument.add_object(oInterfaceB)
     oDeviceA.add_interface(oInterfaceA)
     oDeviceA.add_interface(oInterfaceB)
+    oDeviceA.add_component(oComponentA)
     oDeviceB.add_interface(oInterfaceA)
+    oDeviceB.add_component(oComponentA)
+    oDeviceB.add_component(oComponentB)
 
     # Create the HTML
     oDocument.create_html()
